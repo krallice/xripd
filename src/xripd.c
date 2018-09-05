bb86b34356503b9387e6e612ce3f556af696ea4b
krallice <sroz9960@gmail.com>
2018-09-05 10:51:59 +0000

    xripd_settings_t init

--- src/xripd.c
+++ src/xripd.c
@@ -1,7 +1,46 @@
 #include "xripd.h"
 
+// xripd Defines:
+#define XRIPD_PASSIVE_IFACE "eth1"
+
+// Protocol Defines:
+#define RIP_MCAST_IP 224.0.0.9
+
+typedef struct xripd_settings_t {
+	int sd; // Socket Descriptor
+	char iface_name[IFNAMSIZ]; // Human String for an interface, ie. "eth3" or "enp0s3"
+} xripd_settings_t;
+
+int init_socket(xripd_settings_t *xripd_settings) {
+	return 0;
+}
+
+// Initialise our xripd_settings_t struct:
+xripd_settings_t *init_xripd_settings() {
+
+	// Init and Zeroise:
+	xripd_settings_t *xripd_settings = (xripd_settings_t*)malloc(sizeof(xripd_settings_t));
+	memset(xripd_settings, 0, sizeof(xripd_settings_t));
+
+	// Check interface string length, and copy if safe:
+	if ( strlen(XRIPD_PASSIVE_IFACE) <= IFNAMSIZ ) {
+		strcpy(xripd_settings->iface_name, XRIPD_PASSIVE_IFACE);
+	} else {
+		return NULL;
+	}	
+
+	return xripd_settings;
+}
+
 int main(void) {
 
-	return 0;
+	xripd_settings_t *xripd_settings = init_xripd_settings();
+	if ( xripd_settings == NULL ) {
+		return 1;
+	}
 
+	if ( init_socket(xripd_settings) == 0)
+		return 1;
+
+	return 0;
 }
--- src/xripd.h
+++ src/xripd.h
@@ -1,6 +1,22 @@
 #ifndef XRIPD_H
 #define XRIPD_H
 
+// Standard Includes:
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <limits.h>
+#include <unistd.h>
+
+// Network Specific:
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <linux/if_packet.h>
+#include <linux/if_ether.h>
+#include <linux/if_arp.h>
+#include <arpa/inet.h>
 
 
 
