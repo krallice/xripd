536d4c16238c629122ee3d48d13eb3e910e1a9ac
krallice <sroz9960@gmail.com>
2018-10-08 05:49:12 +0000

    route files, empty for now

---
+++ src/route.h
@@ -0,0 +1,23 @@
+#ifndef XRIPD_ROUTE_H
+#define XRIPD_ROUTE_H
+
+#include "route.h"
+
+// Standard Includes:
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <limits.h>
+#include <unistd.h>
+
+// Network Specific:
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <linux/if_packet.h>
+#include <linux/if_ether.h>
+#include <linux/if_arp.h>
+#include <arpa/inet.h>
+
+#endif
