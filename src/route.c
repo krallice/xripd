4cb52d3104c4cbfd9a963797e2880af52f98e661
krallice <sroz9960@gmail.com>
2018-10-09 09:44:51 +0000

    init NETLINK socket

--- src/route.c
+++ src/route.c
@@ -1,6 +1,19 @@
 #include "route.h"
 
+// Function to create our netlink interface (used to install routes etc..):
 int init_netlink(xripd_settings_t *xripd_settings) {
+
+	// Spawn our NETLINK_ROUTE AF_NETLINK socket
+	xripd_settings->nlsd = socket(AF_NETLINK, SOCK_RAW, NETLINK_ROUTE);
+	if ( xripd_settings->nlsd <= 0 ) {
+		fprintf(stderr, "[route]: Error, Unable to open AF_NETLINK Socket..\n");
+		close(xripd_settings->nlsd);
+		return 1;
+	} else {
+#if XRIPD_DEBUG == 1
+		fprintf(stderr, "[route]: Initialised AF_NETLINK Socket.\n");
+#endif
+	}
 
 	// Success:
 	return 0;
