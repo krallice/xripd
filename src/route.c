8d15689fea690e57b9329907e25fba47f21caa83
krallice <sroz9960@gmail.com>
2018-10-19 11:16:55 +0000

    Better comments in route.c

--- src/route.c
+++ src/route.c
@@ -135,9 +135,14 @@ int netlink_install_new_route(xripd_settings_t *xripd_settings, rib_entry_t *ins
 
 void dump_rtm_newroute(xripd_settings_t *xripd_settings, struct nlmsghdr *nlhdr) {
 
-	// Pointer to our rtmsg. Each rtmsg may contain multiple attributes:
-	struct rtmsg *route_entry;
+	// Each Netlink datagram may contain 1+ route_attributes, followed by route data
+	//
+	//                       NLMSG_DATA                              NLMSG_NEXT
+	//  * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+	//  * |  netlink header |  rtmsg | rtattr | rtattr  | rtattr  |  netlink header |
+	//  * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 	struct rtattr *route_attribute;
+	struct rtmsg *route_entry;
 	int len = 0; // Attribute length
 
 	// Presentation strings for dst_ip & gw_ip:
@@ -198,51 +203,77 @@ int netlink_add_local_routes_to_rib(xripd_settings_t *xripd_settings) {
 	struct {
 		struct nlmsghdr nl;
 		struct rtgenmsg rt_gen;
-		char buf[8192];
 	} req;
 
 	char buf[8192];
 
+	// Netlink socket address for the kernel itself.
+	// Referencing this struct in our msghdr struct allows our netlink request
+	// to be delivered to kernel:
 	struct sockaddr_nl kernel_address;
 	
-	// Sending datastructures:
+	// Used in sendmsg()
+	// Our NLM_F_REQUEST will be contained in these structs:
+	// Containers our rtnetlink messages:
 	struct msghdr rtnl_msghdr;
 	struct iovec io_vec;
 
-	// Reply datastructures:
+	// Structs for our returned data from the kernel:
 	struct msghdr rtnl_msghdr_reply;
 	struct iovec io_vec_reply;
 
+	// Pointer to netlink message header:
 	struct nlmsghdr *msg_ptr;
 	
 	int end_parse = 0;
 
+	// Zero out our sending datastructures for sendmsg():
 	memset(&kernel_address, 0, sizeof(kernel_address));
 	memset(&rtnl_msghdr, 0, sizeof(rtnl_msghdr));
 	memset(&io_vec, 0, sizeof(io_vec));
-
 	memset(&req, 0, sizeof(req));
 
 #if XRIPD_DEBUG == 1
 	fprintf(stderr, "[route]: Sending NLM_F_DUMP request to kernel.\n");
 #endif
-
-	// Kernel's address for NETLINK sockets:
+	// Kernel's address for NETLINK sockets
+	// kernel_address.nl_pid = 0 is implicit:
 	kernel_address.nl_family = AF_NETLINK;
 	kernel_address.nl_groups = 0;
 
-	req.nl.nlmsg_len = NLMSG_LENGTH(sizeof(struct rtgenmsg));
+	// Our netlink header:
+	/*
+	 *  0		    8               16              24              32
+	 *  1 2 3 4 5 6 7 8 1 2 3 4 5 6 7 8 1 2 3 4 5 6 7 8 1 2 3 4 5 6 7 8
+	 * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+ 
+	 * |                     message length (32)			   |
+	 * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+ 
+	 * |       type(16)                |            flags(16)          |
+	 * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+ 
+	 * |                     sequence number (32)                      |
+	 * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+ 
+	 * |                             pid (32)                          |
+	 * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+ 
+	 */
+
+	req.nl.nlmsg_len = NLMSG_LENGTH(sizeof(struct rtgenmsg)); // Header is going to carry a rtgenmsg
 	req.nl.nlmsg_type = RTM_GETROUTE;
-	req.nl.nlmsg_flags = NLM_F_REQUEST | NLM_F_DUMP;
+	req.nl.nlmsg_flags = NLM_F_REQUEST | NLM_F_DUMP; // Dump the entire routing table
 	req.nl.nlmsg_seq = 1;
-	req.nl.nlmsg_pid = getpid();
+	req.nl.nlmsg_pid = getpid(); // Our sending 'address'
+
 	req.rt_gen.rtgen_family = AF_INET;
 
+	// Pack our msgheader
+	// Iovector that references our actual netlink data:
 	io_vec.iov_base = &req;
 	io_vec.iov_len = req.nl.nlmsg_len;
 
+	// Reference our newly created iovector:
+	rtnl_msghdr.msg_iovlen = 1; // Only one buffer to send
 	rtnl_msghdr.msg_iov = &io_vec;
-	rtnl_msghdr.msg_iovlen = 1;
+
+	// Destined for the kernel:
 	rtnl_msghdr.msg_name = &kernel_address;
 	rtnl_msghdr.msg_namelen = sizeof(kernel_address);
 
@@ -252,20 +283,31 @@ int netlink_add_local_routes_to_rib(xripd_settings_t *xripd_settings) {
 
 		int len;
 
+		// Zeroise our reply structs:
 		memset(&rtnl_msghdr_reply, 0, sizeof(rtnl_msghdr_reply));
 		memset(&io_vec_reply, 0, sizeof(io_vec_reply));
 	
+		// Place our recieved data into buf:
 		io_vec_reply.iov_base = buf;
 		io_vec_reply.iov_len = sizeof(buf);
 
 		rtnl_msghdr_reply.msg_iov = &io_vec_reply;
 		rtnl_msghdr_reply.msg_iovlen = 1;
+
+		// Receive from the kernel:
 		rtnl_msghdr_reply.msg_name = &kernel_address;
 		rtnl_msghdr_reply.msg_namelen = sizeof(kernel_address);
 
+		// Give it to me:
 		len = recvmsg(xripd_settings->nlsd, &rtnl_msghdr_reply, 0);
+
+		// Got something?
 		if (len) {
-			for (msg_ptr = (struct nlmsghdr *) buf; NLMSG_OK(msg_ptr, len); msg_ptr = NLMSG_NEXT(msg_ptr, len)) {
+
+			msg_ptr = (struct nlmsghdr *) buf;
+
+			// Is it safe to parse other netlink macros?
+			while  (NLMSG_OK(msg_ptr, len) ) {
 
 				switch (msg_ptr->nlmsg_type) {
 
@@ -273,6 +315,7 @@ int netlink_add_local_routes_to_rib(xripd_settings_t *xripd_settings) {
 						end_parse = 1;
 						break;
 
+					// Kernel's sent us a route:
 					case RTM_NEWROUTE:
 #if XRIPD_DEBUG == 1
 						dump_rtm_newroute(xripd_settings, msg_ptr);
@@ -283,6 +326,11 @@ int netlink_add_local_routes_to_rib(xripd_settings_t *xripd_settings) {
 					default:
 						break;
 				}
+
+				// Walk the chain of responses:
+				// 1 Request (NLM_F_DUMP) in this instance, generates multiple responses
+				// sizeof(buf) is quite large, so the sendmsg() call returns multiple messages:
+				msg_ptr = NLMSG_NEXT(msg_ptr, len);
 			}
 		} else {
 #if XRIPD_DEBUG == 1
